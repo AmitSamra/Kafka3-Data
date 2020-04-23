def2da109f7a767134c9dfa21b4a448ca240512a
AmitSamra <Amit.Samra@outlook.com>
2020-04-23 01:43:18 -0400

    Finished Phase 1

--- phase1/consumer-to-SQL.py
+++ phase1/consumer-to-SQL.py
@@ -1,5 +1,34 @@
 from kafka import KafkaConsumer, TopicPartition
 from json import loads
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+
+# Load enviornment variables from .env file
+dotenv_local_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=dotenv_local_path, verbose=True) 
+
+
+# Create Declartive base class maintains catalog of classes and tables
+Base = declarative_base()
+
+
+# Create Transaction class; transaction table mapped to this class
+# transaction table stores records for end-users of our application
+class Transaction(Base):
+    __tablename__ = 'transaction'
+    __table_args__ = {"schema": "kafka"}
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    custid = Column(Integer)
+    type = Column(String(250), nullable=False)
+    date = Column(Integer)
+    amt = Column(Integer)
+
 
 class XactionConsumer:
     def __init__(self):
@@ -16,7 +45,10 @@ class XactionConsumer:
         # THE PROBLEM is every time we re-run the Consumer, ALL our customer
         # data gets lost!
         # add a way to connect to your database here.
-
+        self.engine = create_engine('postgresql://' + os.environ.get('POSTGRES_USER') + ':' + os.environ.get('POSTGRES_PASSWORD') + '@localhost:5433/postgres')
+        Session = sessionmaker(bind=self.engine)
+        Session.configure(bind=self.engine)
+        self.session = Session()    
         #Go back to the readme.
 
     def handleMessages(self):
@@ -25,6 +57,10 @@ class XactionConsumer:
             print('{} received'.format(message))
             self.ledger[message['custid']] = message
             # add message to the transaction table in your SQL usinf SQLalchemy
+            message_sql = Transaction(custid=message['custid'], type=message['type'], date=message['date'], amt=message['amt'])
+            self.session.add(message_sql)
+            self.session.commit()
+
             if message['custid'] not in self.custBalances:
                 self.custBalances[message['custid']] = 0
             if message['type'] == 'dep':
@@ -32,6 +68,7 @@ class XactionConsumer:
             else:
                 self.custBalances[message['custid']] -= message['amt']
             print(self.custBalances)
+
 
 if __name__ == "__main__":
     c = XactionConsumer()
